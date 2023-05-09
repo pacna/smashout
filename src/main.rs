ec81e41b8c7b992dedb7a828eab0c731bc0909a8
pacna <31336845+pacna@users.noreply.github.com>
2023-05-08 23:37:39 -0400

    add README and bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Smashout".into(),
+            resolution: (1280.0, 720.0).into(),
+            ..default()
+        }),
+        ..default()
+    }))
+    .run();
 }
